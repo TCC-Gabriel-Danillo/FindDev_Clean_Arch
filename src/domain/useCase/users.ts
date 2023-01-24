002feae37f26ad03ece1c771cb21aaf8f27aeed6
Danillo Souza <danillosouza1704@gmail.com>
2023-01-23 21:27:03 -0300

    Ädicionado página de autenticação

--- src/domain/useCase/users.ts
+++ src/domain/useCase/users.ts
@@ -1,6 +1,13 @@
 import { AuthResponse } from "./auth";
-import { Position } from "./Position";
+import { Coords, Position } from "./Position";
 
 export interface User extends AuthResponse {
   position: Position;
+}
+
+export interface UserUseCase {
+  listUsersByDistance: (location: Coords, distanceInM: number) => Promise<User[]>;
+  getUserPosition: () => Promise<Position>;
+  createUser: (user: AuthResponse) => Promise<User>;
+  updateUser: (user: User) => Promise<void>;
 }
--- src/presentation/components/button/index.tsx
+++ src/presentation/components/button/index.tsx
@@ -1,20 +1,19 @@
-import { ReactNode } from 'react';
-import { TouchableOpacity, TouchableOpacityProps, ViewStyle } from 'react-native';
-import { Text } from "../text"
-import { styles } from "./styles"
+import { ReactNode } from "react";
+import { TouchableOpacity, TouchableOpacityProps, ViewStyle } from "react-native";
+import { Text } from "../text";
+import { styles } from "./styles";
 
 interface Props extends TouchableOpacityProps {
-    children: ReactNode
-    style?: ViewStyle
+  children: ReactNode;
+  style?: ViewStyle;
 }
 
 export const Button: React.FC<Props> = ({ children, style, ...rest }) => {
-  return(
+  return (
     <TouchableOpacity {...rest} style={[styles.button, style]}>
-        <Text fontWeight='semibold' style={styles.buttonText}>
-            {children}
-        </Text>
+      <Text fontWeight="semibold" style={styles.buttonText}>
+        {children}
+      </Text>
     </TouchableOpacity>
-  )
-}
-
+  );
+};
