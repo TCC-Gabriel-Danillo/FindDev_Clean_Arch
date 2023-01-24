4c3f18c8a7c788e796bbce0f7d1398bd2f02d76e
Danillo Souza <danillosouza1704@gmail.com>
2023-01-23 21:22:34 -0300

    adicionado hook de fontes

---
+++ src/presentation/hooks/useCustomFonts.ts
@@ -0,0 +1,16 @@
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
+
+export function useCustomFonts() {
+  const [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+  });
+
+  return [fontsLoaded];
+}
